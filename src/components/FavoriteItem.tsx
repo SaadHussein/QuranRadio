71b4356b9c28512f3fa92e4d3b4cce3cd7a87ee4
SaadHussein <sa3dhusseein@gmail.com>
2023-08-27 00:13:01 +0300

    improve more UI details

--- src/components/FavoriteItem.tsx
+++ src/components/FavoriteItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FavoriteItem: React.FC<{ name: string }> = ({ name }) => {
   return (
-    <div className="shadow-md w-[120px] h-[120px] p-2 flex-grow-0 flex-shrink-0 rounded-md duration-300 bg-blue-950 hover:bg-blue-900 text-white flex items-end">
+    <div className="shadow-md cursor-pointer w-[120px] h-[120px] p-2 flex-grow-0 flex-shrink-0 rounded-md duration-300 bg-blue-950 hover:bg-blue-900 text-white flex items-end">
       {name}
     </div>
   );
