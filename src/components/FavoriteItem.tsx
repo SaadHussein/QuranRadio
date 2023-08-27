f6146a26b7e40ef8397b21c64c45faa99781dfcc
SaadHussein <sa3dhusseein@gmail.com>
2023-08-28 00:29:12 +0300

    fix bug

--- src/components/FavoriteItem.tsx
+++ src/components/FavoriteItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
-import { setCurrentStation } from "../redux/QRadio";
+import { controlPlaying, setCurrentStation } from "../redux/QRadio";
 
 const FavoriteItem: React.FC<{ name: string; url: string; id: string }> = ({
   name,
@@ -11,9 +11,10 @@ const FavoriteItem: React.FC<{ name: string; url: string; id: string }> = ({
 
   return (
     <div
-      onClick={() =>
+      onClick={() => (
+        dispatch(controlPlaying(false)),
         dispatch(setCurrentStation({ name: name, url: url, id: id }))
-      }
+      )}
       className="shadow-md cursor-pointer w-[120px] h-[120px] p-2 flex-grow-0 flex-shrink-0 rounded-md duration-300 bg-blue-950 hover:bg-blue-900 text-white flex items-end"
     >
       {name}
