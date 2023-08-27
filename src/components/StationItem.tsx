8275df2ee325547aca7f094f47dbb78de6483fea
SaadHussein <sa3dhusseein@gmail.com>
2023-08-27 12:11:56 +0300

    yarab ana leh ekhtart elmagal dah and t3bt

--- src/components/StationItem.tsx
+++ src/components/StationItem.tsx
@@ -1,29 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import { FaPlayCircle } from "react-icons/fa";
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { useDispatch } from "react-redux/es/hooks/useDispatch";
-import { stateManagment } from "../model/State";
-import { setIsRunning } from "../redux/QRadio";
 
-const StationItem: React.FC<{ name: string; url: string }> = ({
-  name,
-  url,
-}) => {
-  const isRunning = useSelector(
-    (state: stateManagment) => state.QRadio.isRunning
-  );
-  const dispatch = useDispatch();
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-
-  const audioHandler = () => {};
-
+const StationItem: React.FC<{ name: string; url: string }> = ({ name }) => {
   return (
-    <div
-      className="flex text-white items-center justify-start gap-5 hover:opacity-70 cursor-pointer"
-      onClick={audioHandler}
-    >
+    <div className="flex text-white items-center justify-start gap-5 hover:opacity-70 cursor-pointer">
       <FaPlayCircle size={36} />
-      <audio src={url} autoPlay={false} ref={audioRef}></audio>
       <p className="lg:text-[24px] md:text-[18px] xsm:text-[16px] text-[10px] mr-2 font-bold">
         {name}
       </p>
